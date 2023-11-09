a42259af60f4c9eba2dd76945424963ac73a4447
PathumLD <2020is031@stu.ucsc.cmb.ac.lk>
2023-11-09 14:58:38 +0530

    Add project files.

---
+++ OOP/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Person
+    {
+        public String name;
+        public int age;
+
+        public void eating()
+        {
+
+        }
+
+        public void sleeping()
+        {
+
+        }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating object person1
+            Person person1 = new Person();
+
+            //Creating object person2
+            Person person2 = new Person();
+
+
+            //Accessing properties
+            person1.name = "Pathum";
+            person1.age = 26;
+
+            person2.name = "Michelle";
+            person2.age = 23;
+
+            Console.WriteLine("Name is " + person1.name);
+            Console.WriteLine("Age is " + person1.age);
+            Console.WriteLine("Name is " + person2.name);
+            Console.WriteLine("Age is " + person2.age);
+
+            Console.ReadLine();
+        }
+    }
+}
